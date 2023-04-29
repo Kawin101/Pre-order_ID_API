cd8fef7be783dca2c106ffaf9f28608488651106
Kawinphop Chomnikorn <25kawinphop@gmail.com>
2023-04-29 22:32:09 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,14 +1,24 @@
 function generateAndSaveIds() {
-  var sheet = SpreadsheetApp.getActive().getSheetByName("Sheet1"); // Change "Sheet1" to the name of your sheet
-  var startNumber = 1;
-  var endNumber = 1999;
+  var sheet = SpreadsheetApp.openById("1fKCbWeAwrbpICkqVHMI9cJwHQupxfOBeEEwd1KL-Xzs").getSheetByName("Sheet1"); // Change "Sheet1" to the name of your sheet
+  // var sheet = SpreadsheetApp.getActive().getSheetByName("Sheet1"); // Change "Sheet1" to the name of your sheet
   var idPrefix = "PR-";
   var idLength = 4;
   
-  for (var i = startNumber; i <= endNumber; i++) {
-    var id = idPrefix + padNumberWithZeros(i, idLength);
-    sheet.appendRow([id]); // Change [id] to [id, value1, value2, ...] if you want to save additional data
+  var lastRow = sheet.getLastRow();
+  if (lastRow == 0) { // if there are no existing IDs in the sheet, create the first ID as "PR-0000"
+    var newId = idPrefix + padNumberWithZeros(0, idLength); // create the new ID with the number 0
+    sheet.appendRow([newId]); // add the new ID to the sheet
+    return;
   }
+
+  var lastId = sheet.getRange(lastRow, 1).getValue(); // get the last generated ID from the sheet
+  var lastNumber = parseInt(lastId.substring(3)) || 0; // extract the numeric part of the ID and convert it to a number, defaulting to 0 if it is not a valid number
+  if (lastNumber < 1) lastNumber = 0; // set the lastNumber to 0 if it is less than 1
+
+  var newNumber = lastNumber + 1; // generate the next number in sequence
+  var newId = idPrefix + padNumberWithZeros(newNumber, idLength); // create the new ID with the next number
+  
+  sheet.appendRow([newId]); // add the new ID to the sheet
 }
 
 function padNumberWithZeros(number, length) {
@@ -16,5 +26,6 @@ function padNumberWithZeros(number, length) {
   while (paddedNumber.length < length) {
     paddedNumber = "0" + paddedNumber;
   }
+  console.log(paddedNumber);
   return paddedNumber;
 }
